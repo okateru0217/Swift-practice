1371729899be6fcc6b9c95dbda87315eaeeefa32
okateru0217 <okateru.0217@gmail.com>
2021-02-12 23:12:41 +0900

    Playground

---
+++ Playground/Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by 岡澤輝明 on 2021/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
