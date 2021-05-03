de69ccf7ec6fc49401361f081e0900ac5c1c4c6f
okateru0217 <okateru.0217@gmail.com>
2021-05-03 20:58:52 +0900

    5/3 ユニットテストの学習

---
+++ UnitTest/UnitTestTests/UnitTestTests.swift
@@ -0,0 +1,53 @@
+//
+//  UnitTestTests.swift
+//  UnitTestTests
+//
+//  Created by 岡澤輝明 on 2021/05/03.
+//
+
+import XCTest
+@testable import UnitTest
+
+class UnitTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func testBMI() {
+       let p = Person("hanako", 140, 40)
+       let ret = p.bmi()
+       XCTAssertEqual(ret, 20.41)
+   }
+
+    func testPlus() {
+        let viewController = ViewController()
+        let result = viewController.plus(a: 3, b: 4)
+        XCTAssertEqual(result, 7)
+    }
+}
