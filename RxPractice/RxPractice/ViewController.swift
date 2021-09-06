1f26503e5e7d1517d6b4c54b6db2b69639a2463f
okateru0217 <okateru.0217@gmail.com>
2021-09-06 17:30:39 +0900

    9/6

--- RxPractice/RxPractice/ViewController.swift
+++ RxPractice/RxPractice/ViewController.swift
@@ -11,27 +11,27 @@ import RxCocoa
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UINib(nibName: "RxPracticeTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.register(UINib(nibName: "RxPracticeTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
     
 }
 
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RxPracticeTableViewCell
-        return cell
-    }
-    
-    
-}
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RxPracticeTableViewCell
+//        return cell
+//    }
+//
+//
+//}
