8d53517775211d34216f522a5d276b9820889534
okateru0217 <okateru.0217@gmail.com>
2021-09-08 23:46:45 +0900

    Project: app1_imitation Add:table view

---
+++ app1_imitation/app1_imitation/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  app1_imitation
+//
+//  Created by 岡澤輝明 on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let task = [2, 2]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableViewRegister()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return task.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        return cell
+    }
+    
+    func tableViewRegister() {
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
+    
+    
+}
