4ba78b0118854f1c8f078e64a7e3f9b96e0617ca
okateru0217 <okateru.0217@gmail.com>
2021-09-14 23:07:02 +0900

    Project: app1_imitation Add: Table view list cell

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,84 +1,58 @@
 import UIKit
 
-private var detailRecordItem = [RecordItem]()
+print(0123456789)
+print("a")
 
-struct RecordItem {
-    var item: String
-    var time: Int
+
+var total = 0
+
+func count(n: Int) -> Int {
+    total += n
+    return total
 }
 
-//let record = RecordItem(item: "item", time: 10)
-//detailRecordItem.append(record)
-//print(detailRecordItem)
+func reset() { total = 0 }
 
-struct RecordItems {
-    var total: RecordItem
-    var today: RecordItem
-    
-    init() {
-        total = RecordItem(item: "total", time: 10)
-        today = RecordItem(item: "today", time: 1)
+
+
+func area(height h: Double, width w: Double) -> Double { h * w }
+let a = area(height: 10.0, width: 12.5)
+
+
+//func plusOne(array: [Int]) -> [Int] {
+//    var array = array
+//    for i in 0 ..< array.count {
+//        array[i] = array[i] + 1
+//    }
+//    return array
+//}
+//
+//var oldArray = [0, 1, 2]
+//print(oldArray)
+//var newArray = plusOne(array: oldArray)
+//print(oldArray)
+
+func plusOne(array : inout [Int]) -> [Int] {
+    for i in 0  ..< array.count  {
+        array[i] = array[i] + 1
     }
-}
-var recordItems = RecordItems()
-
-detailRecordItem.append(contentsOf: [recordItems.total, recordItems.today])
-print(detailRecordItem)
-
-// クロージャー
-let double = { (x: Int) -> Int in
-    return x * 2
-}
-double(2)
-
-var plus = { (x: Int) -> Int in
-    var y = 0
-    y += x
-    return x + y
-}
-plus(3)
-
-var multi = { (x: Int, y: Int) -> Int in
-    return x + y
-}
-multi(3, 7)
-
-var multiReturn = { (x: Int, y: Int) -> Int in
-    x + y
-}
-multiReturn(2, 6)
-
-var closure: (String) -> Int
-closure = { string in
-    string.count
-}
-closure("abcd")
-
-// override
-class Fish {
-    var distance = 0
-    func stop() {
-        print("止まりました")
-    }
+    return array
 }
 
-class Karei: Fish {
-    override func stop() {
-        let distance: Int = 3
-        print(distance + 5)
-    }
+var oldArray = [0, 1, 2]
+print(oldArray)                             // => [0, 1, 2]
+var newArray = plusOne(array: &oldArray)    // => [1, 2, 3]
+print(oldArray)
+
+
+func mySwap(_ a: inout Int, _ b: inout Int) {
+    let t = a
+    a = b
+    b = t
 }
 
-class Suzuki: Fish {
-    override func stop() {
-        print("ギャーギャー言いながら")
-        super.stop()
-    }
-}
-
-var fish = Fish()
-fish.stop()
-var karei = Karei()
-karei.stop()
-var suzuki = Suzuki()
-suzuki.stop()
+var x = 100
+var y = 0
+mySwap(&x, &y)
+print(x)
+print(y)
--- try-anything/Pods/Target Support Files/Pods-try-anything-try-anythingUITests/Pods-try-anything-try-anythingUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_try_anything_try_anythingUITests : NSObject
-@end
-@implementation PodsDummy_Pods_try_anything_try_anythingUITests
-@end
