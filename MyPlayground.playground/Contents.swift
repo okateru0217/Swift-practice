7df9d8f2d078b83cd530657654f95adc2c5a147c
okateru0217 <okateru.0217@gmail.com>
2021-09-28 23:24:39 +0900

    9/28

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,10 @@
 import UIKit
 
-class Product {
-    var name: String
-    var price: Int
-    var consumptionTax: Int {
-        get {
-            return Int(Float(price) * 0.1)
-        }
-        set {
-            price = Int(Float(newValue) / 0.1)
-        }
-    }
-
-    init(name: String, price: Int) {
-        self.name = name
-        self.price = price
-    }
+class test {
+    let testLet: UILabel? = nil
 }
 
-var apple = Product(name: "りんご", price: 100)
-apple.consumptionTax
 
-apple.price = 150
-apple.consumptionTax
-
-apple.consumptionTax = 20
-apple.price
+class test1 {
+    var test: String?
+}
